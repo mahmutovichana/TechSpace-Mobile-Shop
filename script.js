d3c47132ec04fdf35ca018a0a7e8de79d5394b8d
Emina Efendić <115942770+efendicemina@users.noreply.github.com>
2023-11-04 12:08:41 +0100

    Adding visit count

--- script.js
+++ script.js
@@ -51,13 +51,21 @@ prev.onclick = function() {
     }
 }
 
+// Dobijanje trenutnog brojača iz lokalnog skladišta (ako postoji)
+let count = localStorage.getItem('visitCount');
 
-const countElement = document.getElementById("count");
-
-prebrojPosjete();
-
-function prebrojPosjete() {
-    fetch('https://api.countapi.xyz/update/pws-stranica/androidShop/?amount=1').
-    then(res => res.json()).
-    then(res => {countElement.innerHTML = res.value;});
+// Provjera da li brojač postoji u lokalnom skladištu
+if (count) {
+  count = parseInt(count);
+} else {
+  count = 0;
 }
+
+// Povećanje brojača za jedan
+count += 1;
+
+// Ažuriranje HTML elementa sa novim brojem
+document.getElementById('count').textContent = count;
+
+// Spremanje brojača nazad u lokalno skladište
+localStorage.setItem('visitCount', count);
